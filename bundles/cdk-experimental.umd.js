a553f0fd7def2a1f78ba35059bfdf923e3fa6b24
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-12 21:38:33 +0000

    master - 0dbe23bb9 fix(menu): allow focus to be moved inside menuClosed event (#18756)

--- bundles/cdk-experimental.umd.js
+++ bundles/cdk-experimental.umd.js
@@ -12,7 +12,7 @@
 	 * found in the LICENSE file at https://angular.io/license
 	 */
 	/** Current version of the CDK Experimental package. */
-	var VERSION = new core.Version('9.1.2-sha-1a3c35a8c');
+	var VERSION = new core.Version('9.1.2-sha-0dbe23bb9');
 
 	/**
 	 * @license
