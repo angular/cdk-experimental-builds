60fe942f608c7a8d9c18386061ba7beec6b1ec44
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-03-31 20:16:56 +0000

    master - 09d0b1f7b refactor(tree): remove renderer usage (#18930)

--- fesm5/cdk-experimental.js
+++ fesm5/cdk-experimental.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the CDK Experimental package. */
-var VERSION = new Version('9.2.0-sha-262e2e49f');
+var VERSION = new Version('9.2.0-sha-09d0b1f7b');
 
 /**
  * @license
