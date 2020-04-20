9285bf25b4befeaa1ac47ee7aea3b8a6e67886d8
Liz Mitchell <asyncliz@gmail.com>
2020-04-20 14:38:05 +0000

    master - 0bd1e4ab7 fix(material-experimental/form-field): add  filled MDC text field class (#19103)

--- fesm2015/cdk-experimental.js
+++ fesm2015/cdk-experimental.js
@@ -8,7 +8,7 @@ import { Version } from '@angular/core';
  * found in the LICENSE file at https://angular.io/license
  */
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('9.2.1-sha-3c99efc98');
+const VERSION = new Version('9.2.1-sha-0bd1e4ab7');
 
 /**
  * @license
