d52e5b57337b6b7af983667757d8406d6660f007
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2020-06-26 04:00:38 +0000

    10.0.x - 952f57631 docs(select): switch examples to fill appearance (#19708)

--- bundles/cdk-experimental-dialog.umd.js
+++ bundles/cdk-experimental-dialog.umd.js
@@ -317,7 +317,7 @@
              * Attaches a DOM portal to the dialog container.
              * @param portal Portal to be attached.
              * @deprecated To be turned into a method.
-             * @breaking-change 10.0.0-sha-d3f287d65
+             * @breaking-change 10.0.0-sha-952f57631
              */
             _this.attachDomPortal = function (portal) {
                 if (_this._portalHost.hasAttached()) {
