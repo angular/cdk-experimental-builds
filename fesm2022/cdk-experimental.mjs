2365281a1be36d159e5e61a8e6f4732919575e94
Wagner Maciel <wagnermaciel@google.com>
2023-07-06 18:06:35 +0000

    16.1.x - 71eb8cbc8 release: cut the v16.1.4 release

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('16.1.3');
+const VERSION = new Version('16.1.4');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
