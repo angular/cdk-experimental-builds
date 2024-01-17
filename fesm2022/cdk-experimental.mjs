627772a3e745c156a9b825d403bbff86f6559d03
Andrew Seguin <andrewseguin@users.noreply.github.com>
2024-01-17 19:30:17 +0000

    17.1.x - 38929b8 Revert "release: cut the v17.1.0 release" (#28440)

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('17.1.0');
+const VERSION = new Version('17.1.0-rc.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
