5ecc5d63448128c939d070103fdff6b233dd9a44
Amy Sorto <8575252+amysorto@users.noreply.github.com>
2024-06-05 19:47:00 +0000

    18.0.x - 7fbdb62 release: cut the v18.0.2 release

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('18.0.1');
+const VERSION = new Version('18.0.2');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
