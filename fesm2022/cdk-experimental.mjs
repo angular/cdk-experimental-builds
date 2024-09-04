18e57d78cec61630bbd016703bf22eb6d50f340b
Miles Malerba <mmalerba@google.com>
2024-09-04 21:13:33 +0000

    main - 13e7a92 release: cut the v19.0.0-next.3 release

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('19.0.0-next.2');
+const VERSION = new Version('19.0.0-next.3');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
