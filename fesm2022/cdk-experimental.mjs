aaeedd0181a9a84254222143759a50a38735094c
Kristiyan Kostadinov <crisbeto@abv.bg>
2024-09-30 17:48:15 +0000

    18.2.x - 0fabf52 fix(material/chips): focus escape not working consistently

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('18.2.5');
+const VERSION = new Version('18.2.6');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
