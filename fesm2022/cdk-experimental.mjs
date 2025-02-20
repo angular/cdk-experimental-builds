75a8a4e38e2c3fd3e1fc1495f0764689e46b97b2
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-02-20 06:22:29 +0000

    main - d7150a4 build: update main branch to v20 (#30524)

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('19.3.0-next.0');
+const VERSION = new Version('20.0.0-next.0');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
