b445148c5480529ebd10fbc29add676e83dcb636
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-05-01 08:26:09 +0000

    19.2.x - 6aff47c build: release script producing duplicate results (#30999)

--- npm_package/fesm2022/cdk-experimental.mjs
+++
@@ -1,7 +0,0 @@
-import { Version } from '@angular/core';
-
-/** Current version of the CDK Experimental package. */
-const VERSION = new Version('19.2.13');
-
-export { VERSION };
-//# sourceMappingURL=cdk-experimental.mjs.map
