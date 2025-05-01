6d13f2ff0b3cc464d166c22e883485f3566be34c
Kristiyan Kostadinov <crisbeto@users.noreply.github.com>
2025-05-01 08:24:45 +0000

    main - 280a233 build: release script producing duplicate results (#30999)

--- npm_package/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Version } from '@angular/core';
-
-/** Current version of the CDK Experimental package. */
-declare const VERSION: Version;
-
-export { VERSION };
--- npm_package/ui-patterns/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { L as ListboxInputs, a as ListboxPattern, O as OptionInputs, b as OptionPattern } from '../listbox.d-QWPLmxRe.js';
-export { S as SignalLike, W as WritableSignalLike, c as convertGetterSetterToWritableSignalLike } from '../list-navigation.d-DfOSxsSp.js';
-export { T as TabInputs, b as TabListInputs, e as TabListPattern, c as TabPanelInputs, d as TabPanelPattern, a as TabPattern } from '../tabs.d-BAo141mw.js';
-import '@angular/core';
