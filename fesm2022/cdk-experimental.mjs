7fd1546800106f55ec445c56d0d604a7ce023821
Kristiyan Kostadinov <crisbeto@abv.bg>
2025-05-01 12:40:40 +0000

    20.0.x - 12f7de8 release: cut the v20.0.0-next.10 release

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('20.0.0-next.9');
+const VERSION = new Version('20.0.0-next.10');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
