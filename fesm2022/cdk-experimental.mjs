0d0d6f01fa573bfa666552605e330d5e79b48594
Andrew Seguin <andrewjs@google.com>
2025-07-09 17:35:34 +0000

    20.0.x - 87df3fe release: cut the v20.0.6 release

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('20.0.5');
+const VERSION = new Version('20.0.6');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
