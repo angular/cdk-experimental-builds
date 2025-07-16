4c7d44c1179c4c2de627389d7dd49b46a6adce5d
Miles Malerba <mmalerba@google.com>
2025-07-16 17:59:03 +0000

    20.1.x - 57c245e release: cut the v20.1.1 release

--- fesm2022/cdk-experimental.mjs
+++ fesm2022/cdk-experimental.mjs
@@ -1,7 +1,7 @@
 import { Version } from '@angular/core';
 
 /** Current version of the CDK Experimental package. */
-const VERSION = new Version('20.1.0');
+const VERSION = new Version('20.1.1');
 
 export { VERSION };
 //# sourceMappingURL=cdk-experimental.mjs.map
